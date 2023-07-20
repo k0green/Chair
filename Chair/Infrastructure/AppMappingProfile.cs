5b628609df8ff308eec7d55bb8e16ca050ae530b
Егор Новик <novikegor3@gmail.com>
2023-07-20 12:32:02 +0300

    [feat] add mapping profile

---
+++ Chair/Infrastructure/AppMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Chair.BLL.Dto.ExecutorService;
+using Chair.DAL.Data.Entities;
+
+namespace Chair.Infrastructure
+{
+    public class AppMappingProfile : Profile
+    {
+        public AppMappingProfile()
+        {
+            CreateMap<ExecutorService, ExecutorServiceDto>()
+                .ForMember(dest => dest.ServiceTypeName, opt => opt.MapFrom(src => src.ServiceType.Name))
+                .ReverseMap();
+        }
+    }
+}
