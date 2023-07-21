cea0ae8044758f2cc735f8e6f85b59d105bee2fd
Егор Новик <novikegor3@gmail.com>
2023-07-21 11:27:16 +0300

    [fix] add & update query for service type

--- Chair.BLL/CQRS/ServiceType/AddServiceTypeQuery.cs
+++ Chair.BLL/CQRS/ServiceType/AddServiceTypeQuery.cs
@@ -1,12 +1,10 @@
-﻿using MediatR;
-using Chair.BLL.Dto.ExecutorService;
-using Chair.BLL.Dto.ServiceType;
-using Chair.DAL.Data.Entities;
+﻿using Chair.BLL.Dto.ServiceType;
+using MediatR;
 
 namespace Chair.BLL.CQRS.ServiceType
 {
-    public class UpdateServiceTypeQuery : IRequest<Unit>
+    public class AddServiceTypeQuery : IRequest<Guid>
     {
-        public ServiceTypeDto ServiceTypeDto { get; set; }
+        public AddServiceTypeDto AddServiceTypeDto { get; set; }
     }
 }
--- Chair.BLL/CQRS/ServiceType/UpdateServiceTypeQuery.cs
+++ Chair.BLL/CQRS/ServiceType/UpdateServiceTypeQuery.cs
@@ -1,12 +1,10 @@
-﻿using MediatR;
-using Chair.BLL.Dto.ExecutorService;
-using Chair.BLL.Dto.ServiceType;
-using Chair.DAL.Data.Entities;
+﻿using Chair.BLL.Dto.ServiceType;
+using MediatR;
 
 namespace Chair.BLL.CQRS.ServiceType
 {
-    public class AddServiceTypeQuery : IRequest<Guid>
+    public class UpdateServiceTypeQuery : IRequest<Unit>
     {
-        public AddServiceTypeDto AddServiceTypeDto { get; set; }
+        public ServiceTypeDto ServiceTypeDto { get; set; }
     }
 }
