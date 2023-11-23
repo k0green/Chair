c66438f901a20c76fc5074c22e9aff4e76802994
Егор Новик <novikegor3@gmail.com>
2023-11-23 20:46:49 +0300

    [migration]

---
+++ Chair.DAL/Migrations/20231108152122_AddChatTable.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chair.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddChatTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Chats",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RecipientId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    SenderId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Chats", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Chats_AspNetUsers_RecipientId",
+                        column: x => x.RecipientId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Chats_AspNetUsers_SenderId",
+                        column: x => x.SenderId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    IsEdited = table.Column<bool>(type: "bit", nullable: false),
+                    IsRead = table.Column<bool>(type: "bit", nullable: false),
+                    ChatId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ReplyId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Messages_Chats_ChatId",
+                        column: x => x.ChatId,
+                        principalTable: "Chats",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Messages_Messages_ReplyId",
+                        column: x => x.ReplyId,
+                        principalTable: "Messages",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chats_RecipientId",
+                table: "Chats",
+                column: "RecipientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chats_SenderId",
+                table: "Chats",
+                column: "SenderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_ChatId",
+                table: "Messages",
+                column: "ChatId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_ReplyId",
+                table: "Messages",
+                column: "ReplyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages");
+
+            migrationBuilder.DropTable(
+                name: "Chats");
+        }
+    }
+}
---
+++ Chair.DAL/Migrations/20231109071808_ReplaceSenderAndRecipientIdToMessageTable.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chair.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReplaceSenderAndRecipientIdToMessageTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Chats_AspNetUsers_RecipientId",
+                table: "Chats");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Chats_AspNetUsers_SenderId",
+                table: "Chats");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Chats_RecipientId",
+                table: "Chats");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Chats_SenderId",
+                table: "Chats");
+
+            migrationBuilder.DropColumn(
+                name: "RecipientId",
+                table: "Chats");
+
+            migrationBuilder.DropColumn(
+                name: "SenderId",
+                table: "Chats");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RecipientId",
+                table: "Messages",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SenderId",
+                table: "Messages",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_RecipientId",
+                table: "Messages",
+                column: "RecipientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_SenderId",
+                table: "Messages",
+                column: "SenderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_AspNetUsers_RecipientId",
+                table: "Messages",
+                column: "RecipientId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_AspNetUsers_SenderId",
+                table: "Messages",
+                column: "SenderId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_AspNetUsers_RecipientId",
+                table: "Messages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_AspNetUsers_SenderId",
+                table: "Messages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Messages_RecipientId",
+                table: "Messages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Messages_SenderId",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "RecipientId",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "SenderId",
+                table: "Messages");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RecipientId",
+                table: "Chats",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SenderId",
+                table: "Chats",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chats_RecipientId",
+                table: "Chats",
+                column: "RecipientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chats_SenderId",
+                table: "Chats",
+                column: "SenderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Chats_AspNetUsers_RecipientId",
+                table: "Chats",
+                column: "RecipientId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Chats_AspNetUsers_SenderId",
+                table: "Chats",
+                column: "SenderId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
