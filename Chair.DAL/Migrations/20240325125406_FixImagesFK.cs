d2fd43f06e57cc4c1549b8ddbe2a0ff1d4a946fb
Егор Новик <novikegor3@gmail.com>
2024-03-29 17:05:44 +0300

    [migration]

---
+++ Chair.DAL/Migrations/20240325125406_FixImagesFK.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chair.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixImagesFK : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Images_ExecutorServices_ObjectId",
+                table: "Images");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Images_ObjectId",
+                table: "Images");
+
+            migrationBuilder.DropColumn(
+                name: "ImageURL",
+                table: "ExecutorProfiles");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ExecutorServiceId",
+                table: "MinioFiles",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ExecutorServiceId",
+                table: "Images",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ImageId",
+                table: "ExecutorProfiles",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MinioFiles_ExecutorServiceId",
+                table: "MinioFiles",
+                column: "ExecutorServiceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Images_ExecutorServiceId",
+                table: "Images",
+                column: "ExecutorServiceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExecutorProfiles_ImageId",
+                table: "ExecutorProfiles",
+                column: "ImageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ExecutorProfiles_MinioFiles_ImageId",
+                table: "ExecutorProfiles",
+                column: "ImageId",
+                principalTable: "MinioFiles",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Images_ExecutorServices_ExecutorServiceId",
+                table: "Images",
+                column: "ExecutorServiceId",
+                principalTable: "ExecutorServices",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MinioFiles_ExecutorServices_ExecutorServiceId",
+                table: "MinioFiles",
+                column: "ExecutorServiceId",
+                principalTable: "ExecutorServices",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ExecutorProfiles_MinioFiles_ImageId",
+                table: "ExecutorProfiles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Images_ExecutorServices_ExecutorServiceId",
+                table: "Images");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_MinioFiles_ExecutorServices_ExecutorServiceId",
+                table: "MinioFiles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MinioFiles_ExecutorServiceId",
+                table: "MinioFiles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Images_ExecutorServiceId",
+                table: "Images");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ExecutorProfiles_ImageId",
+                table: "ExecutorProfiles");
+
+            migrationBuilder.DropColumn(
+                name: "ExecutorServiceId",
+                table: "MinioFiles");
+
+            migrationBuilder.DropColumn(
+                name: "ExecutorServiceId",
+                table: "Images");
+
+            migrationBuilder.DropColumn(
+                name: "ImageId",
+                table: "ExecutorProfiles");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageURL",
+                table: "ExecutorProfiles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Images_ObjectId",
+                table: "Images",
+                column: "ObjectId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Images_ExecutorServices_ObjectId",
+                table: "Images",
+                column: "ObjectId",
+                principalTable: "ExecutorServices",
+                principalColumn: "Id");
+        }
+    }
+}
---
+++ Chair.DAL/Migrations/20240325164304_AddExecutorServiceFile.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chair.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExecutorServiceFile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExecutorServiceFiles",
+                columns: table => new
+                {
+                    ProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    MinioFileId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExecutorServiceFiles", x => new { x.ProductId, x.MinioFileId });
+                    table.ForeignKey(
+                        name: "FK_ExecutorServiceFiles_ExecutorServices_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "ExecutorServices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ExecutorServiceFiles_MinioFiles_MinioFileId",
+                        column: x => x.MinioFileId,
+                        principalTable: "MinioFiles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExecutorServiceFiles_MinioFileId",
+                table: "ExecutorServiceFiles",
+                column: "MinioFileId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExecutorServiceFiles");
+        }
+    }
+}
