f4528e0c7c9e0a4fe7b4b68ce7f0b7d496fa977b
Егор Новик <novikegor3@gmail.com>
2024-07-05 13:39:26 +0300

    [migrations]

---
+++ Chair.DAL/Migrations/20240525121859_AddCoordinatesInExecutorService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chair.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCoordinatesInExecutorService : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Lat",
+                table: "ExecutorServices",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Lng",
+                table: "ExecutorServices",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Lat",
+                table: "ExecutorServices");
+
+            migrationBuilder.DropColumn(
+                name: "Lng",
+                table: "ExecutorServices");
+        }
+    }
+}
