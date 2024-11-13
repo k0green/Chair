34ed3ad4e7ecc4c9284873af4f96711829856169
Егор Новик <novikegor3@gmail.com>
2024-11-13 12:40:49 +0300

    feat: add migration

---
+++ Chair.DAL/Migrations/20241111122430_AddParentIdInServiceType.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chair.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddParentIdInServiceType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ParentId",
+                table: "ServiceTypes",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ServiceTypes_ParentId",
+                table: "ServiceTypes",
+                column: "ParentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ServiceTypes_ServiceTypes_ParentId",
+                table: "ServiceTypes",
+                column: "ParentId",
+                principalTable: "ServiceTypes",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ServiceTypes_ServiceTypes_ParentId",
+                table: "ServiceTypes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ServiceTypes_ParentId",
+                table: "ServiceTypes");
+
+            migrationBuilder.DropColumn(
+                name: "ParentId",
+                table: "ServiceTypes");
+        }
+    }
+}
